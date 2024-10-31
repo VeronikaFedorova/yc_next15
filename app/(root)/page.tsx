ba334e479d05ab3a32ed4f80d852991455e2322e
Veronika Fedorova <fedorovavv03@gmail.com>
2024-10-31 17:04:17 +0200

    Add a StartupCard component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
-import SearchForm from "@/components/SearchForm";
+import SearchForm from '@/components/SearchForm';
+import StartupCard from '@/components/StartupCard';
 
-export default async function Home({searchParams}: {
-  searchParams: Promise<{query?: string}>
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: 'K. Rowling' },
+      _id: 1,
+      description: 'The great hall',
+      image: 'https://d.newsweek.com/en/full/1033479/mcdhapo-ec030.jpg',
+      category: 'Hogwarts',
+      title: 'Hogwarts hall',
+    },
+  ];
   return (
     <>
       <section className='pink_container'>
@@ -16,6 +31,21 @@ export default async function Home({searchParams}: {
         </p>
 
         <SearchForm query={query} />
+      </section>
+      <section className='section_container'>
+        <p className='text-30-semibold'>
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className='mt-7 card_grid'>
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className='no-results'>No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
